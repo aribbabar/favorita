66c4f7599b549cfd96aac02e0a29e2852f25dd8a
Arib Farooqui <aribbabar100@gmail.com>
2023-02-11 21:13:55 -0500

    added twitter card tags and open graph tags

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,7 +14,8 @@ const firebaseConfig = {
   projectId: "favorita-8c0ce",
   storageBucket: "favorita-8c0ce.appspot.com",
   messagingSenderId: "1027799895335",
-  appId: "1:1027799895335:web:e3cfe4de2a732e02786d02"
+  appId: "1:1027799895335:web:e3cfe4de2a732e02786d02",
+  measurementId: "G-HHD6ZZMKPN"
 };
 
 // Initialize Firebase
@@ -22,5 +24,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const analytics = getAnalytics(app);
 
 export { auth, db, storage };
